2db3b1c42328fcfe7a80b65fc1bf9cb03d8860f8
Diego Sosa <dhsosa98@gmail.com>
2022-05-21 13:48:52 -0300

    format file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,10 +5,14 @@ import { DatabaseModule } from './database/modules/db.module';
 import { PackagesModule } from './packages/modules/packages.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { ControlTourismModule } from './tourismControl/controlTourism.module';
 
 @Module({
-  imports: [PackagesModule, UsersModule, DatabaseModule, ControlTourismModule, ConfigModule.forRoot()],
+  imports: [
+    PackagesModule,
+    UsersModule,
+    DatabaseModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
