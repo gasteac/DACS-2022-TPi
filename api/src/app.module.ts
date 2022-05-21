ed618413dd99ee23a0b5401540ab038fa672c064
Gowther <AcevedoAriel@outlook.com>
2022-05-21 19:52:05 -0300

    linux case sensitive

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/modules/db.module';
-import { PackagesModule } from './packages/modules/packages.module';
+import { PackagesModule } from './packages/modules/Packages.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 
