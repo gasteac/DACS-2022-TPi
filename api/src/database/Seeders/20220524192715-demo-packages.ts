ec12c9b0a56f886517b009755ccf426b916a3f78
JoaquinRamirez98 <joaquin.ramirez170998@gmail.com>
2022-05-24 17:47:02 -0300

    dale gaston loco

--- api/src/database/Seeders/20220524192715-demo-packages.ts
+++ api/src/database/Seeders/20220524192715-demo-packages.ts
@@ -11,7 +11,7 @@ module.exports = {
     insuranceId: 1, 
     showId: 1,
     total: 1.0,
-  }  
+  } ,
   {
     name: 'Sex time starter pack',
     quantPeople: 4,
@@ -20,7 +20,7 @@ module.exports = {
     insuranceId: 2, 
     showId: 2,
     total: 2.0,
-  }
+  },
   {
     name: 'Sex time starter pack',
     quantPeople: 4,
