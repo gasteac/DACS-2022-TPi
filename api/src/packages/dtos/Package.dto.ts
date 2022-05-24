5e86dc361859f777dad88a5fa63a42f55d3571cd
Acevedo Ariel <AcevedoAriel@outlook.com>
2022-05-23 22:40:14 -0300

    Unificado de package dto
    no es necesario un nuevo archivo, solo hay que agregarlo dentro del principal

--- api/src/packages/dtos/Package.dto.ts
+++ api/src/packages/dtos/Package.dto.ts
@@ -23,3 +23,29 @@ export class PackageDto {
   @IsNumber()
   showId: number;
 }
+
+export class PackageOnUpdateDto {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsNumber()
+  quantPeople: number;
+
+  @IsOptional()
+  @IsNumber()
+  hotelId: number;
+
+  @IsOptional()
+  @IsNumber()
+  ticketId: number;
+
+  @IsOptional()
+  @IsNumber()
+  insuranceId: number;
+
+  @IsOptional()
+  @IsNumber()
+  showId: number;
+}
